8eafc2c81d5a4b30ca12c1d7ac391715847ec8dc
Jerry Asher <ja2038@gmail.com>
2020-07-16 09:08:07 -0700

    can't quite get auth to work with twitter

--- A08 - Authentication/start/src/App.js
+++ A08 - Authentication/start/src/App.js
@@ -1,23 +1,25 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 import SiteHeader from './components/SiteHeader';
 import Dashboard from './pages/Dashboard';
 import Home from './pages/Home';
+// import PrivateRoute from './components/PrivateRoute';
 import './App.css';
+import { useAuth0 } from './contexts/auth0-context';
 
 export default function App() {
   return (
     <Router>
-      <div className="app">
+      <div className='app'>
         {/* site header */}
         <SiteHeader />
 
         {/* routes */}
         <Switch>
-          <Route path="/dashboard">
+          <Route path='/dashboard'>
             <Dashboard />
           </Route>
-          <Route path="/" exact={true}>
+          <Route path='/' exact={true}>
             <Home />
           </Route>
         </Switch>
--- A08 - Authentication/start/src/components/SiteHeader copy.js
+++ A08 - Authentication/start/src/components/SiteHeader copy.js
@@ -6,11 +6,11 @@ export default function SiteHeader() {
   const { isAuthenticated, login, logout, user } = useContext(Auth0Context);
 
   return (
-    <div className='site-header'>
+    <div className="site-header">
       {/* stuff on the left */}
       <div>
-        <Link to='/'>Home</Link>
-        <Link to='/dashboard'>Dashboard</Link>
+        <Link to="/">Home</Link>
+        <Link to="/dashboard">Dashboard</Link>
       </div>
 
       {/* stuff on the right */}
