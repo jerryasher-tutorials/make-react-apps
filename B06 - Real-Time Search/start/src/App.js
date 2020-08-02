62203d749675707939f1b9c974a04ed8defd1d17
Jerry Asher <ja2038@gmail.com>
2020-08-02 00:03:47 -0700

    algolia search;

--- B06 - Real-Time Search/start/src/App.js
+++ B06 - Real-Time Search/start/src/App.js
@@ -1,4 +1,14 @@
 import React from 'react';
+
+import algoliasearch from 'algoliasearch/lite';
+import {
+  InstantSearch,
+  SearchBox,
+  Hits,
+  Pagination,
+  Stats,
+} from 'react-instantsearch-dom';
+
 import './App.css';
 
 // ===== OPTION 1: if you want to create your own algolia account and fill it with data =====
@@ -10,10 +20,36 @@ import './App.css';
 // api key: ae8a3f7cff93d40282a4831930bfa38b
 // index name: products
 
+const api_id = `4YBNWI4SXR`;
+const api_key = `34265bd5abe239d41831f1f5a381070d`;
+const index_name = `products`;
+
+const searchClient = algoliasearch(api_id, api_key);
+
 export default function App() {
   return (
-    <div className="app">
-      <div className="search-container"></div>
-    </div>
+    <InstantSearch searchClient={searchClient} indexName={index_name}>
+      <div className="app">
+        <div className="search-container">
+          <Stats />
+          <SearchBox />
+          <Hits hitComponent={Product} />
+          <Pagination />
+        </div>
+      </div>
+    </InstantSearch>
+  );
+}
+
+function Product({ hit }) {
+  return (
+    <a className="product" href={hit.url} target="_blank">
+      <img rel="noopener noreferrer" src={hit.image} alt={hit.name} />
+      <div>
+        <h3>{hit.brand}</h3>
+        <h2>{hit.name}</h2>
+        <p>${hit.price}</p>
+      </div>
+    </a>
   );
 }
