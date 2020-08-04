d30be8f75a1586a6d6cf75a8fa6fb33a2b4f8cf1
Jerry Asher <ja2038@gmail.com>
2020-08-03 23:01:10 -0700

    GeoForm done

--- B07 - Chart the Weather/start/src/App.js
+++ B07 - Chart the Weather/start/src/App.js
@@ -1,18 +1,42 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './App.css';
+import GeoForm from './components/GeoForm';
 
 // openweathermap.org
+// course:
 // https://api.openweathermap.org/data/2.5/onecall?lat=33.441792&lon=-94.037689&exclude=hourly,minutely&units=imperial&appid=bb96c7f9ac6f57dc00333727c5407547
 // api key: bb96c7f9ac6f57dc00333727c5407547
 
-// google maps api
-// api key: AIzaSyDp4os33WF6-4d-xFVyL0HsUUHN7dOml_w
+const openweather_api_key = process.env.REACT_APP_OPENWEATHER_API_KEY;
+const openweather_endpoint = 'api.openweathermap.org/data/2.5';
+
+const home_lat = 37.7618;
+const home_long = -122.44586;
+
+// https://api.openweathermap.org/data/2.5/onecall?lat=37.761800&lon=-122.445860&exclude=hourly,minutely&units=imperial&appid=349147899309bf857db33d928042ed78
 
 export default function App() {
+  const [latLong, setLatLong] = useState({ status: null, lat: '', long: '' });
+  // 1. have a form, type in a city
+  // 2. geocode to convert city to lat long
+  // 3. pass lat long to weather to get weather data
+  // 4. format it for chart
+  // 5. display the chart
+
+  let latLongStr = '';
+  if (latLong.status) {
+    latLongStr = `${latLong.status} -> ${latLong.lat}, ${latLong.long}`;
+  }
+
   return (
     <div className="app">
       {/* form goes here */}
+      <GeoForm updateLatLong={setLatLong} />
+      <div>LatLong: {latLongStr}</div>
       {/* chart goes here */}
+      {latLong && <WeatherChart latLong={latLong} />}
     </div>
   );
 }
+// component to take lat long, get weather, display chart
+const WeatherChart = ({ latLong }) => <>chart</>;
---
+++ B07 - Chart the Weather/start/src/components/GeoForm.js
@@ -0,0 +1,75 @@
+import React, { useState, useEffect } from 'react';
+
+// google maps api
+// api key: AIzaSyDp4os33WF6-4d-xFVyL0HsUUHN7dOml_w
+const geocode_api_key = process.env.REACT_APP_GOOGLE_GEOCODING_API_KEY;
+const geocode_endpoint = 'https://maps.googleapis.com/maps/api/geocode/json';
+
+// component to input city from form, convert to lat long
+// https://developers.google.com/maps/documentation/geocoding/overview?hl=en_US
+// https://stackoverflow.com/questions/tagged/google-geocoding-api
+export default function GeoForm({ updateLatLong }) {
+  const [address, setAddress] = useState('4521 17th St., San Francisco 94114');
+  const [lookup, setLookup] = useState(false);
+
+  useEffect(() => {
+    if (address && lookup) {
+      geoCodeAddress();
+    }
+    setLookup(false);
+  }, [address, lookup]);
+
+  async function geoCodeAddress() {
+    const query = `address=${encodeURI(address)}&key=${geocode_api_key}`;
+
+    const response = await fetch(`${geocode_endpoint}?${query}`, {
+      // headers: { Authorization: `Bearer ${token}` },
+    });
+    const data = await response.json();
+    // we have data!
+    console.log('GeoForm: geoCodeAddress: ', data);
+    console.dir(data);
+
+    const {
+      status,
+      results: [{ address_components, formatted_address, geometry }],
+    } = data;
+
+    console.log('GeoForm -> status', status);
+
+    console.log('GeoForm -> geometry', geometry);
+    console.log('GeoForm -> formatted_address', formatted_address);
+    console.log('GeoForm -> address_components', address_components);
+
+    const { lat = '', lng: long = '' } = geometry.location;
+    console.log('GeoForm -> lat', lat);
+    console.log('GeoForm -> long', long);
+
+    const latLong = {
+      status,
+      lat,
+      long,
+    };
+    console.log('GeoForm -> latLong', latLong);
+
+    updateLatLong(latLong);
+  }
+
+  const lookupAddress = (e) => {
+    e.preventDefault();
+    console.log('address', address);
+    setLookup(true);
+  };
+
+  return (
+    <form onSubmit={lookupAddress}>
+      <input
+        type="text"
+        placeholder="address"
+        value={address}
+        onChange={(e) => setAddress(e.target.value)}
+      />
+      <button>locate</button>
+    </form>
+  );
+}
